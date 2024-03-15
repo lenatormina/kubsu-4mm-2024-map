0f9c1e0fc76c8a8359b4ef2de9887b638e193506
Tormina Elena <lena.tormina2001@mail.ru>
2024-03-15 13:19:06 -0400

    ng new1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LeafletModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kubsu-4mm-2024-map';
+  
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
 }
